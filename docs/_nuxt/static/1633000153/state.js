ffb5f313fdc36847673a0fa64cae07eb73d32c24
Luca Iaconelli <lucabelli@msn.com>
2021-09-30 13:28:49 +0200

    docs: updated github pages

--- docs/_nuxt/static/1633000153/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-humans-txt\u002F_nuxt\u002Fstatic\u002F1633000153",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-humans-txt\u002F",assetsPath:"\u002Fnuxt-humans-txt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
